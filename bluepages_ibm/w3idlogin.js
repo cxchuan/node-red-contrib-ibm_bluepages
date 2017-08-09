f8a1be2a44eabf499518288f6b40f9f7a2c1b077
cxchuan <vitomychueng@163.com>
2017-08-09 14:07:04 +0800

    tiny change for test
    tiny change for test

--- bluepages_ibm/w3idlogin.js
+++ bluepages_ibm/w3idlogin.js
@@ -7,18 +7,19 @@ module.exports=function(RED){
        // this.email=config.email;
         var node=this;
         node.on('input',function(msg){
-            msg.payload.account=msg.payload.account;
+            msg.payload.account=msg.payload.account+"how are you?";
             msg.payload.password=msg.payload.password;
    //         msg.payload= msg.payload.toLowerCase();
-            bluepages.authenticate(msg.payload.account,msg.payload.password,function(data){
-           if(data){
-        console.log(data);
-        node.send("true")
-        //node.json(200,{url:"/home"});//define url/home
-        }else
-     // node.sendStatus(404);
-      node.send("false");
-        });
+    //         bluepages.authenticate(msg.payload.account,msg.payload.password,function(data){
+    //        if(data){
+    //     console.log(data);
+    //     node.send("true")
+    //     //node.json(200,{url:"/home"});//define url/home
+    //     }else
+    //  // node.sendStatus(404);
+    //   node.send("false");
+    //     });
+          node.send(msg);
         });
     }
      RED.nodes.registerType("w3idlogin",w3idlogin);
