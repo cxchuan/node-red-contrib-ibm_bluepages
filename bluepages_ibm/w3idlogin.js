806c72bb19d83c0da3c28862dd932710a93c9cd0
cxchuan <vitomychueng@163.com>
2017-08-09 22:27:01 +0800

    tiny change node msg
    tiny change node msg for debug

--- bluepages_ibm/w3idlogin.js
+++ bluepages_ibm/w3idlogin.js
@@ -9,18 +9,19 @@ module.exports=function(RED){
         node.on('input',function(msg){
             msg.payload.account=msg.payload.account;
             msg.payload.password=msg.payload.password;
-            //node.send(msg);
-            bluepages.authenticate(msg.payload.account,msg.payload.password,function(data){
-           if(data){
-            msg.payload.account=msg.payload.account+"yes!";
-            console.log(data);
+
             node.send(msg);
-        //node.json(200,{url:"/home"});//define url/home
-            }else
-     // node.sendStatus(404);
-            msg.payload.account=msg.payload.account+"no!";
-            node.send(msg);
-        });
+    //         bluepages.authenticate(msg.payload.account,msg.payload.password,function(data){
+    //        if(data){
+    //         msg.payload.account=msg.payload.account+"yes!";
+    //         console.log(data);
+    //         node.send(msg);
+    //     //node.json(200,{url:"/home"});//define url/home
+    //         }else
+    //  // node.sendStatus(404);
+    //         msg.payload.account=msg.payload.account+"no!";
+    //         node.send(msg);
+    //     });
         });
     }
      RED.nodes.registerType("w3idlogin",w3idlogin);
