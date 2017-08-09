bf218952811b3b498f0944867c3fa7d1e77098c1
cxchuan <vitomychueng@163.com>
2017-08-09 14:21:54 +0800

    tiny change to test module
    test the module bluepages

--- bluepages_ibm/w3idlogin.js
+++ bluepages_ibm/w3idlogin.js
@@ -7,19 +7,20 @@ module.exports=function(RED){
        // this.email=config.email;
         var node=this;
         node.on('input',function(msg){
-            msg.payload.account=msg.payload.account+"how are you?";
+            msg.payload.account=msg.payload.account;
             msg.payload.password=msg.payload.password;
-   //         msg.payload= msg.payload.toLowerCase();
-    //         bluepages.authenticate(msg.payload.account,msg.payload.password,function(data){
-    //        if(data){
-    //     console.log(data);
-    //     node.send("true")
-    //     //node.json(200,{url:"/home"});//define url/home
-    //     }else
-    //  // node.sendStatus(404);
-    //   node.send("false");
-    //     });
-          node.send(msg);
+            bluepages.authenticate(msg.payload.account,msg.payload.password,function(data){
+           if(data){
+       msg.payload.account=msg.payload.account+"yes!";
+        console.log(data);
+        node.send(msg);
+        //node.json(200,{url:"/home"});//define url/home
+        }else
+     // node.sendStatus(404);
+     msg.payload.account=msg.payload.account+"no!";
+      node.send(msg);
+        });
+         // node.send(msg);
         });
     }
      RED.nodes.registerType("w3idlogin",w3idlogin);
